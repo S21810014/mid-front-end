8c52d8b8499ff0f1ae61bde97968d3fd6ed1825d
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2021-10-06 22:01:30 +0800

    implemented adding student to table

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import createListOption from './create_list_option.js'
+import addStudentToTable from './add_student_to_table.js'
 
 const dropdownData = {
     'Pascasarjana' : [
@@ -34,8 +35,41 @@ const dropdownData = {
     ],
 }
 
+//pre-initialize studentsTable with sample students
+
+
 const programOfStudyDropdown = document.getElementById('addStudFormPOS')
 const facultyDropdown = document.getElementById('addStudFormFac')
+const addStudentBtn = document.getElementById('addStudFormAddBtn')
+const studentsTable = document.getElementById('studentsTable')
+
+const students = [
+    {
+        studentID: '105021810001',
+        fullName: 'John Doe',
+        gender: 'Male',
+        faculty: 'Fakultas Ilmu Komputer',
+        programOfStudy: 'Sistem Informasi',
+    },
+    {
+        studentID: '105021810002',
+        fullName: 'Alice',
+        gender: 'Female',
+        faculty: 'Fakultas Keperawatan',
+        programOfStudy: 'Profesi Ners',
+    },
+    {
+        studentID: '105021810004',
+        fullName: 'Timothy Tiwow',
+        gender: 'Male',
+        faculty: 'Fakultas Ilmu Komputer',
+        programOfStudy: 'Informatika',
+    }
+]
+
+students.forEach(element => {
+    addStudentToTable(element, studentsTable)
+});
 
 facultyDropdown.addEventListener('change', e => {
     const selectedFaculty = e.target.value
@@ -43,10 +77,24 @@ facultyDropdown.addEventListener('change', e => {
     //clear the dropdown before adding data
     programOfStudyDropdown.innerHTML = null
 
-    console.log(createListOption)
-
     //add the program of study based on selected faculty
     for(const programOfStudy of dropdownData[selectedFaculty]) {
         programOfStudyDropdown.appendChild(createListOption(programOfStudy))
     }
+})
+
+addStudentBtn.addEventListener('click', e => {
+    const studentID = document.getElementById('addStudFormStudID').value
+    const studentName = document.getElementById('addStudFormStudName').value
+    const studentGender = document.querySelector('input[name="genderRadioBtn"]:checked').id
+    const studentFaculty = facultyDropdown.value
+    const studentProgramOfStudy = programOfStudyDropdown.value
+
+    addStudentToTable({
+        studentID: studentID,
+        studentName: studentName,
+        studentGender: studentGender == 'genderRadioBtnMale' ? 'Male' : 'Female',
+        studentFaculty: studentFaculty,
+        studentProgramOfStudy: studentProgramOfStudy
+    }, studentsTable)
 })
