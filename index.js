b4fdb6d4c4528153d21bd4ada247fa602f22dd9c
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2021-10-06 20:58:04 +0800

    setup dictionary for dropdown data

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("hello world")
+const dropdownData = {
+    'Pascasarjana' : [
+        'Magister Manajemen',
+        'Magister Teologi'
+    ],
+    'Fakultas Filsafat' : [
+        'Ilmu Filsafat',
+    ],
+    'Fakultas Keguruan dan Ilmu Pendidikan' : [
+        'Pendidikan Agama',
+        'Pendidikan Bahasa Inggris',
+        'Pendidikan Ekonomi',
+        'Pendidikan Luar Sekolah',
+    ],
+    'Fakultas Ekonomi dan Bisnis' : [
+        'Akuntansi',
+        'Manajemen',
+    ],
+    'Fakultas Pertanian' : [
+        'Agroteknologi',
+    ],
+    'Fakultas Ilmu Komputer' : [
+        'Informatika',
+        'Sistem Informasi',
+    ],
+    'Fakultas Keperawatan' : [
+        'Profesi Ners',
+        'Keperawatan',
+    ],
+    'Akademi Sekretari Manajemen Indonesia Klabat' : [
+        'Sekretari (D3)',
+    ],
+}
