40f50d292ca5187e06150c4ab727d3052162bc5d
Timothy Merfry Tiwow <74723356+S21810014@users.noreply.github.com>
2021-10-07 21:21:32 +0800

    implemented input validation (#8)
    * add popover placeholder to input boxes
    
    * impelemented input validation
    
    * implemented resetting input form after adding student
    
    resolve #3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import createListOption from './create_list_option.js'
 import addStudentToTable from './add_student_to_table.js'
+import setupValidation from './setup_validation.js'
 
 const dropdownData = {
     'Pascasarjana' : [
@@ -33,6 +34,15 @@ const dropdownData = {
     'Akademi Sekretari Manajemen Indonesia Klabat' : [
         'Sekretari (D3)',
     ],
+
+    //need to add this default value because resetting the faculty dropdown will trigger 'change'
+    //event, and if after that we modify the program of study dropdown, it might not work
+    //
+    //so we added this option to make sure that if the faculty dropdown is reset back to default value,
+    //we are also resetting the program of study dropdown
+    '-- SELECT FACULTY --' : [
+        '-- SELECT PROGRAM OF STUDY --',
+    ]
 }
 
 //pre-initialize studentsTable with sample students
@@ -42,6 +52,10 @@ const programOfStudyDropdown = document.getElementById('addStudFormPOS')
 const facultyDropdown = document.getElementById('addStudFormFac')
 const addStudentBtn = document.getElementById('addStudFormAddBtn')
 const studentsTable = document.getElementById('studentsTable')
+
+//queries div with 'addStudFormCard' class name, then query all child div with 'inputValidWrapper' class name
+const needValidInputs = document.getElementsByClassName("addStudFormCard")[0].querySelectorAll(".inputValidWrapper")
+setupValidation(needValidInputs)
 
 const students = [
     {
@@ -90,6 +104,36 @@ addStudentBtn.addEventListener('click', e => {
     const studentFaculty = facultyDropdown.value
     const studentProgramOfStudy = programOfStudyDropdown.value
 
+    for(const input of Array.from(needValidInputs)) {
+        const inputElem = input.children[0]
+        const inputClasses = Array.from(inputElem.classList)
+        const popOver = input.children[1]
+
+        if(inputClasses.includes('noLetter'))
+            //regex testing
+            /\D/.test(inputElem.value) || inputElem.value.length < 1 ?
+                popOver.style.display = null : popOver.style.display = "none"
+
+        if(inputClasses.includes('noNumeric'))
+            //regex testing
+            /\d/.test(inputElem.value) || inputElem.value.length < 1 ?
+                popOver.style.display = null : popOver.style.display = "none"
+
+        if(inputClasses.includes('noDefaultSelected')) {
+            //had to do this since we can't be sure that the combobox hasn't been modified
+            const defaultValue = ['-- SELECT FACULTY --', '-- SELECT PROGRAM OF STUDY --']
+
+            defaultValue.includes(inputElem.value) ?
+                popOver.style.display = null : popOver.style.display = "none"
+        }
+    }
+
+    //if any pop overs still visible, do not continue execution
+    for(const input of Array.from(needValidInputs))
+        if(input.children[1].style.display !== 'none')
+            return
+
+
     addStudentToTable({
         studentID: studentID,
         studentName: studentName,
@@ -97,4 +141,13 @@ addStudentBtn.addEventListener('click', e => {
         studentFaculty: studentFaculty,
         studentProgramOfStudy: studentProgramOfStudy
     }, studentsTable)
+
+    //reset all inputs
+
+    //reset inputs except the dropdown menus
+    for(const input of Array.from(needValidInputs).splice(0, needValidInputs.length - 2))
+        input.children[0].value = ''
+    
+    //manually reset faculty dropdown menu
+    facultyDropdown.value = '-- SELECT FACULTY --'
 })
